20ad04b487993a5e93eac3c95e41788433701c05
Hasan Sarker <devops@psellbd.com>
2025-10-30 07:31:25 +0600

    chore(showbox): bump provider version to 1.3 and update catalog

--- dist/showbox/catalog.js
+++ dist/showbox/catalog.js
@@ -3,16 +3,36 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.genres = exports.catalog = void 0;
 exports.catalog = [
     {
-        title: "Home",
-        filter: "",
-    },
-    {
         title: "Movies",
         filter: "/movie",
     },
     {
         title: "TV Shows",
         filter: "/tv",
+    },
+    {
+        title: "4K",
+        filter: "/movie?quality=4K&release_year=all&genre=all&rating=all",
+    },
+    {
+        title: "Romance",
+        filter: "/movie?genre=16",
+    },
+    {
+        title: "Hindi",
+        filter: "/movie?country=India",
+    },
+    {
+        title: "Action",
+        filter: "/movie?genre=1",
+    },
+    {
+        title: "Animation",
+        filter: "/movie?genre=3",
+    },
+    {
+        title: "Horror",
+        filter: "/movie?genre=13",
     },
 ];
 exports.genres = [];
--- providers/showbox/catalog.ts
+++ providers/showbox/catalog.ts
@@ -1,15 +1,35 @@
 export const catalog = [
   {
-    title: "Home",
-    filter: "",
-  },
-  {
     title: "Movies",
     filter: "/movie",
   },
   {
     title: "TV Shows",
     filter: "/tv",
+  },
+  {
+    title: "4K",
+    filter: "/movie?quality=4K&release_year=all&genre=all&rating=all",
+  },
+  {
+    title: "Romance",
+    filter: "/movie?genre=16",
+  },
+  {
+    title: "Hindi",
+    filter: "/movie?country=India",
+  },
+  {
+    title: "Action",
+    filter: "/movie?genre=1",
+  },
+  {
+    title: "Animation",
+    filter: "/movie?genre=3",
+  },
+  {
+    title: "Horror",
+    filter: "/movie?genre=13",
   },
 ];
 
